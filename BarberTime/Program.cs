a60004e801f124dc19452b4722c982dff44f70f8
Luan Carvalho <luancarlos0476@gmail.com>
2026-04-09 09:54:14 -0300

    feat: configure DbContext

--- BarberTime/Program.cs
+++ BarberTime/Program.cs
@@ -1,7 +1,16 @@
+using BarberTime.Data;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+builder.Services.AddDbContext<BarberTimeContext>(Options =>
+Options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
+
 
 var app = builder.Build();
 
