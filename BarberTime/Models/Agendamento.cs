221a0671965ae03bd3fd04d26a911add5bd11ee2
Luan Carvalho <luancarlos0476@gmail.com>
2026-04-09 09:29:31 -0300

    Feat: create Agendamento model with data annotations validation

--- BarberTime/Models/Agendamento.cs
+++ BarberTime/Models/Agendamento.cs
@@ -1,3 +1,26 @@
 using System.ComponentModel.DataAnnotations;
 
-namespace
+namespace BarberTime.Models;
+
+public class Agendamento
+{
+    public int Id { get; set; }
+
+    [Required(ErrorMessage = "O nome do cliente é obrigatório")]
+    [StringLength(100)]
+    public string NomeCliente { get; set; } = string.Empty;
+
+    [Required(ErrorMessage = "O telefone é obrigatório")]
+    [StringLength(20)]
+    public string telefone { get; set; } = string.Empty;
+
+    [Required(ErrorMessage = "A data e hora são obrigatórios")]
+    public DateTime DataHora { get; set; }
+
+    [Required(ErrorMessage = "O serviço é obrigatório")]
+    [StringLength(50)]
+    public string Serviço { get; set; } = string.Empty;
+
+    [StringLength(50)]
+    public string? Observacoes {get; set;}
+}
